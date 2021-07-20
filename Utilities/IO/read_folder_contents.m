5d034f601df4f983f5837375b0ae77a13393c6ab
Robert F Cooper <robert.cooper@marquette.edu>
2021-07-20 15:46:37 -0500

    Updated commenting for "read_folder_contents".

--- Utilities/IO/read_folder_contents.m
+++ Utilities/IO/read_folder_contents.m
@@ -1,29 +1,52 @@
-function [ file_list isdir numfiles ] = read_folder_contents( root_dir,extension, wild_card )
+function [ file_list, isdir, numfiles ] = read_folder_contents( root_dir,extension, wildcard )
+% function [ file_list, isdir, numfiles ] = READ_FOLDER_CONTENTS( root_dir, extension, wildcard )
 % Robert Cooper 12 - 05 - 09
-% This function extracts all the filenames from a folder with the desired
-% extension.
-% v2: an optional output argument is whether or not the path is directory
-%     This is of the same length and is associated directly with a file
-% Input:
-% @ root_dir:
-% This argument is the root directory that one wishes to be read.
-% @ extension:
-% This argument allows the caller to specify the extension for which he/she
-% is looking.
+% This function extracts all filenames from a folder into a cell array.
+%
+% [ file_list, isdir, numfiles ] = READ_FOLDER_CONTENTS( root_dir )
+%       Returns an Nx1 cell array of character vectors (file_list);
+%       each cell contains the full path to a file in the directory
+%       specified (root_dir).
+%
+%       'isdir' is a corresponding Nx1 vector that contains boolean values
+%       if a given path is a directory (true) or a file (false).
+%
+%       'numfiles' is the total number of files that are detected in the
+%       directory.
+%       
+%
+% [ ... ] = READ_FOLDER_CONTENTS( root_dir, extension )
+%       Including an character vector with JUST the extension of the files of
+%       interest will only return the above for files that match that
+%       extension.
+%
+%       For example: READ_FOLDER_CONTENTS( 'C:\Windows', 'dll'); will
+%       return a cell array of all dll files in the windows directory.
+%
+% [ ... ] = READ_FOLDER_CONTENTS( root_dir, extension, wildcard )
+%       Including an character vector with the extension and wildcard
+%       of the files of interest will only return the above for files that
+%       match that wildcard and extension.
+%
+%       For example: READ_FOLDER_CONTENTS( 'C:\Windows', 'dll', 'py'); will
+%       return a cell array of all dll files in the Windows directory that
+%       contain the string 'py'.
+%
+
 
 
 x=1;
-if ~exist('wild_card','var')
-    wild_card = '*';
+if ~exist('wildcard','var')
+    wildcard = '*';
 else
-    wild_card = ['*' wild_card '*'];
+    wildcard = ['*' wildcard '*'];
 end
 
 if ~exist('extension','var')
-    file_list=dir(fullfile(root_dir, wild_card) );
+    file_list=dir(fullfile(root_dir, wildcard) );
     file_list=file_list(3:end);
 else
-    file_list=dir(fullfile(root_dir,[wild_card extension]));
+    file_list=dir(fullfile(root_dir,[wildcard extension]));
 end
 numoffiles=length(file_list);
 
